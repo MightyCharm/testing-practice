cfa5c7296a956bc01ba60968cac5f2929261efd8
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2026-05-19 21:12:38 +0200

    feat(reverseString): add reverseString function with tests

--- src/project.js
+++ src/project.js
@@ -2,4 +2,8 @@ function capitalize(word) {
   return word[0].toUpperCase() + word.slice(1);
 }
 
-export { capitalize };
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
+export { capitalize, reverseString };
--- src/project.test.js
+++ src/project.test.js
@@ -1,13 +1,23 @@
-import { capitalize } from "./project.js";
+import { capitalize, reverseString } from "./project.js";
 
+// tests for capitalize function
 test('capitalizes first letter of "water" to "Water"', () => {
-  expect(capitalize("water")).toMatch("Water");
+  expect(capitalize("water")).toBe("Water");
 });
 
 test('capitalizes first letter of "hello" to "Hello"', () => {
-  expect(capitalize("hello")).toMatch("Hello");
+  expect(capitalize("hello")).toBe("Hello");
 });
 
 test('capitalizes first letter of "peter" to "Peter"', () => {
-  expect(capitalize("peter")).toMatch("Peter");
+  expect(capitalize("peter")).toBe("Peter");
+});
+
+// test for reverseString function
+test('reverses string "hello world" to "dlrow olleh"', () => {
+  expect(reverseString("hello world")).toBe("dlrow olleh");
+});
+
+test('reverses string "The Odin Project" to "tcejorP nidO ehT"', () => {
+  expect(reverseString("The Odin Project")).toBe("tcejorP nidO ehT");
 });
