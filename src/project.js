ed40bb5f0405bce2e1c30117de939aba05ffea97
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2026-05-19 22:31:29 +0200

    feat(calculator): add basic operations with tests

--- src/project.js
+++ src/project.js
@@ -6,4 +6,22 @@ function reverseString(string) {
   return string.split("").reverse().join("");
 }
 
-export { capitalize, reverseString };
+const calculator = {
+  add(num1, num2) {
+    return num1 + num2;
+  },
+
+  subtract(num1, num2) {
+    return num1 - num2;
+  },
+
+  divide(num1, num2) {
+    return num1 / num2;
+  },
+
+  multiply(num1, num2) {
+    return num1 * num2;
+  },
+};
+
+export { capitalize, reverseString, calculator };
--- src/project.test.js
+++ src/project.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./project.js";
+import { capitalize, reverseString, calculator } from "./project.js";
 
 // tests for capitalize function
 test('capitalizes first letter of "water" to "Water"', () => {
@@ -13,11 +13,28 @@ test('capitalizes first letter of "peter" to "Peter"', () => {
   expect(capitalize("peter")).toBe("Peter");
 });
 
-// test for reverseString function
+// tests for reverseString function
 test('reverses string "hello world" to "dlrow olleh"', () => {
   expect(reverseString("hello world")).toBe("dlrow olleh");
 });
 
 test('reverses string "The Odin Project" to "tcejorP nidO ehT"', () => {
   expect(reverseString("The Odin Project")).toBe("tcejorP nidO ehT");
+});
+
+// tests for calculator object
+test("adds 3 and 4 to equal 7", () => {
+  expect(calculator.add(3, 4)).toBe(7);
+});
+
+test("subtracts 5 from 10 to equal 5", () => {
+  expect(calculator.subtract(10, 5)).toBe(5);
+});
+
+test("divides 8 by 2 to equal 4", () => {
+  expect(calculator.divide(8, 2)).toBe(4);
+});
+
+test("multiplies 3 and 4 to equal 12", () => {
+  expect(calculator.multiply(3, 4)).toBe(12);
 });
